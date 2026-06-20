eb9646ead162ac0f8f181972cba996ee59da2b8b
Asmual <Asmual04@gmail.com>
2026-06-21 00:05:32 +0600

    Update CORS configuration and credentials for Render and Vercel integration.

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const connectDB = require('./config/db');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://arthub-three.vercel.app'
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 async function startServer() {
   try {
     const db = await connectDB();
+    console.log("MongoDB connected successfully");
 
     app.get('/', (req, res) => {
       res.send('ArtHub Server is running... ');
@@ -19,6 +27,9 @@ async function startServer() {
 
     const artistCollection = db.collection('artists');
 
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port} `);
+    });
 
   } catch (error) {
     console.error("Failed to start server:", error);
@@ -26,7 +37,3 @@ async function startServer() {
 }
 
 startServer();
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port} `);
-});
